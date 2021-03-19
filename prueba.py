e9730054089f520cce2bbde64ad83be94947f6ea
Relderf <relderfeg@gmail.com>
2021-03-19 19:29:09 -0300

    Vamos a probar

---
+++ prueba.py
@@ -0,0 +1,9 @@
+linea = "*" * 35
+menu = """ Menú de opciones:
+            1.- Jugar
+            2.- Configurar el juego
+            3.- Salir
+        """
+print(linea)
+print(menu)
+print(linea)
