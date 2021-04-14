7e369dbb61a31cb6526d5a7dcda91655c10c0a36
Relderf <relderfeg@gmail.com>
2021-04-14 19:55:35 -0300

    Desafio sobre pelis 2020 y 5 paises mas productores

---
+++ desafio_clase_5.py
@@ -0,0 +1,30 @@
+import csv
+from collections import Counter
+
+arc_netflix = open("netflix_titles.csv", "r", encoding='utf-8')
+arc_pelis = open("pelis_2020.csv", "w", encoding='utf-8')
+csvreader = csv.reader(arc_netflix, delimiter=',')
+
+pelis_2020 = filter(lambda x: x[1] == "Movie" and x[7] == "2020", csvreader)
+writer = csv.writer(arc_pelis)
+for peli in pelis_2020:
+    writer.writerow([peli[2]])
+
+#Puede ser un error volver a abrirlo para refrescarlo, solo eso se me ocurrio
+arc_netflix = open("netflix_titles.csv", "r", encoding='utf-8') 
+
+csvreader = csv.reader(arc_netflix, delimiter=',')
+lista =[]
+for pais in csvreader:
+    lista.append(pais[5])
+top_5 = Counter(lista).most_common(5)
+print("Los 5 paises que más películas produjeron son: ")
+
+for p in top_5:
+    print(p)
+#No sé por qué toma un espacio vacío "" como País, quizás no le estoy pudiendo indicar bien
+#cuándo terminar y cuenta hasta vaya uno a saber donde.
+
+
+arc_netflix.close()
+arc_pelis.close()
